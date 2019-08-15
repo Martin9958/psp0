2a630714a9d1d518fa3191c0fd634c80b540f427
martin <andres.cantor@payulatam.com>
2019-08-15 18:16:21 -0500

    Inicial implementation

--- src/main/java/edu/escuelaing/arem/model/calculatorKitModel/calculatorKit.java
+++ src/main/java/edu/escuelaing/arem/model/calculatorKitModel/calculatorKit.java
@@ -1,9 +1,29 @@
 package edu.escuelaing.arem.model.calculatorKitModel;
 
+import edu.escuelaing.arem.model.linkedListModel.MyLinkedList;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class calculatorKit {
     /**
      *
      */
-    public calculatorKit(){
+    public calculatorKit(String path){
+        BufferedReader bufferedReader;
+        try{
+            bufferedReader = new BufferedReader(new FileReader(path));
+            MyLinkedList myLinkedList = new MyLinkedList();
+            String line = bufferedReader.readLine();
+            while(line != null){
+                line = line.trim();
+                myLinkedList.addANode(Integer.parseInt(line));
+            }
+            System.out.println(myLinkedList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
